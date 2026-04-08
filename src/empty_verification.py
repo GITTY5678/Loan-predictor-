704120d5dbbe19af74df305ebc038d39f4a698eb
GITTY5678 <rge7311@gmail.com>
2026-04-08 18:45:43 +0530

    code process optimized

--- src/empty_verification.py
+++ src/empty_verification.py
@@ -15,13 +15,17 @@ def missing_values(df):
         print("----------------------------------------------------------------------")
         
 
-def column_variants(df):
+def column_variants(df,columns=None):
     """
     This function takes a dataframe as input and returns the number of unique values in each column.
     It prints like this:
     Column name: number of unique values
     unique values : list of unique values in the column
+    and also if columns are given as input then it will only return excluding that columns
+    the columns parameter is a list of column names to be excluded from the analysis
     """
+    if columns is not None:
+        df = df[[col for col in df.columns if col not in columns]]
     for column in df.columns:
         unique_values=df[column].unique()
         print(f"{column}:{len(unique_values)} unique values")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from empty_verification import missing_values, column_variants
 import pandas as pd
 
+def pass_column_more_unique(df):
+    """ This function takes a dataframe as input and returns the column names in a list which has more than 10 unique values"""
+    unique_values=[]
+    for column in df.columns:
+        if df[column].nunique()>=10:
+            unique_values.append(column) 
+    return unique_values
+    
 df=pd.read_csv("C:\\Users\\HARIHARA SUTHAN\\Documents\\GitHub\\Loan-predictor-\\data\\raw\\train_data.csv")
 missing_values(df)
-column_variants(df)
+column_variants(df,pass_column_more_unique(df))
 
+
+    
 if __name__ == "__main__":
     print("This is the main function of the program")
-    
